f6e8dcb2376acf8f75ff2979f29a4b0488ce1754
Hsu Peng Jun <hsu.pengjun@icloud.com>
2016-01-23 01:11:30 +0800

    修改 Header 讓 Angular 2 可以傳送 Request

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 // Allow Origin
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "token, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
 
