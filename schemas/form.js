10ad92edf78dfc0ec0831e74b5db78e09aa79457
Hsu Peng Jun <hsu.pengjun@icloud.com>
2016-02-07 21:29:43 +0800

    註解縮排

--- schemas/form.js
+++ schemas/form.js
@@ -11,8 +11,8 @@ var formsSchema = new Schema({
     identifier: String,  // 內部編號，例如「ID-D-005」
     revision: Number,    // 表單格式版本
     signigtures: Number, // 簽核人數
-    group: Number, // 填表群組
-    secret: Number, // 機密等級
+    group: Number,       // 填表群組
+    secret: Number,      // 機密等級
     template: String,
     fields: [Field]
 });
